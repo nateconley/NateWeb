2639fbdeb7dfbccc6bfa7cdc399121daebe57d5a
Nathan Conley <nateconley123@gmail.com>
2016-02-14 23:31:18 -0800

    added sections to landing page and styled some elements

--- mailer.js
+++ mailer.js
@@ -1,1 +1,21 @@
 var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport();
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '<nateconley.com>', // sender address
+    to: 'nateconley123@gmail.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world 🐴', // plaintext body
+    html: '<b>Hello world 🐴</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
