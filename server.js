2fcd631f532664d1e2a6f21ffce42f3d1b6c7512
Nathan Conley <nateconley123@gmail.com>
2016-02-15 14:53:29 -0800

    Added jade template and changed server to express

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-var index = fs.readFileSync('./html/index.html');
+// var index = fs.readFileSync('./html/index.html');
 
-var server = http.createServer(function(request, response){
-	response.writeHead(200, {'Content-type': 'text/html'});
-	response.write(index);
-	response.end();
+// var server = http.createServer(function(request, response){
+// 	response.writeHead(200, {'Content-type': 'text/html'});
+// 	response.write(index);
+// 	response.end();
+// });
+
+// server.listen(3000, function(){
+// 	console.log("server running on port 3000");
+// })
+'use strict';
+
+var express = require('express'),
+			   fs = require('fs');
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("<h1>Hello World!</h1>");
 });
 
-server.listen(3000, function(){
-	console.log("server running on port 3000");
-})
+app.listen(3000, function(){
+	console.log("Frontend server running on port 3000...");
+});
