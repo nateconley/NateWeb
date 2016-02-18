e8e0e2fcb0e13778146102eba049b3b854108e50
Nathan Conley <nateconley123@gmail.com>
2016-02-18 12:32:38 -0800

    Changed mailer again....back to nodemailer

--- mailer.js
+++ mailer.js
@@ -1,19 +1,36 @@
-var postmark = require("postmark");
-var client = new postmark.Client("xxxxxxx");
- 
-var contact = function(req, res){
-	client.sendEmail({
-	  "From": "info@nateconley.com", 
-	  "To": "nateconley123@gmail.com", 
-	  "Subject": "Website Contact Form", 
-	  "TextBody": req.body.message + " " + req.body.email
-	}, function(error, success) {
-    if(error) {
-	    	console.log(error);
-        res.render('contact', { error: true });
-        return;
-    }
-    res.render('contact', { error: false });
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+var contact = function(req, res) {
+	// Setup nodemailer transport
+	var smtpTrans = nodemailer.createTransport(smtpTransport({
+	    service: 'Gmail',
+	    auth: {
+	        user: "me@gmail.com",
+	        pass: "application-specific-password" 
+	      }
+	    }
+    ));
+
+	// Mail options
+	var mailOptions = {
+	    from: 'nateconley123@gmail.com', // sender address 
+	    to: 'nateconley123@gmail.com', // the same mail = want to send it to myself
+	    subject: 'Website Contact Form', // Subject line 
+	    html: '<h2>' + req.body.email + '</h2><br><p>' + req.body.message + '</p>' // html body 
+	};
+
+
+
+	smtpTrans.sendMail(mailOptions, function(error, info){
+	  if(error){
+	  	console.log(error);
+	    res.render('contact', { error: true });
+	    return;
+	  }
+	  // Success
+	  console.log('Message sent: ' + info.response);
+	  res.render('contact', { error: false });
 	});
 }
 
