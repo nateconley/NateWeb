7c4d4a0561c19ba95fdcc80bc2d87fd15b9b3df9
Nathan Conley <nateconley123@gmail.com>
2016-02-18 10:25:21 -0800

    Changed mailer to postmark

--- mailer.js
+++ mailer.js
@@ -1,38 +1,15 @@
-// var sendgrid  = require('sendgrid');
-
-// var contact = function (req, res) {
-
-// 	var emailAddress = req.body.email;
-//   var message = req.body.message;
-
-// 	sendgrid.send({
-// 	  to:       'nateconley123@gmail.com',
-// 	  from:     emailAddress,
-// 	  subject:  'Website Contact Form',
-// 	  text:     message
-// 	}, function(err, json) {
-// 	  if (err) { 
-// 	  	return res.render('contact', { error: true }); 
-// 	  } 
-// 	  res.render('contact', { error: false })
-// 	});
-// }
-
-
-
-
-
 var postmark = require("postmark");
-var client = new postmark.Client("<server key>");
+var client = new postmark.Client("xxxxxxx");
  
 var contact = function(req, res){
 	client.sendEmail({
-	  "From": req.body.email, 
+	  "From": "info@nateconley.com", 
 	  "To": "nateconley123@gmail.com", 
 	  "Subject": "Website Contact Form", 
-	  "TextBody": req.body.message
+	  "TextBody": req.body.message + " " + req.body.email
 	}, function(error, success) {
     if(error) {
+	    	console.log(error);
         res.render('contact', { error: true });
         return;
     }
@@ -40,4 +17,4 @@ var contact = function(req, res){
 	});
 }
 
-module.exports.contact = contact
+module.exports.contact = contact;
