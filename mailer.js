f78dd5f2b5b055e180958c87cc56b0d0497343f9
Nathan Conley <nateconley123@gmail.com>
2016-02-17 16:36:48 -0800

    Changed mailer to sendgrid

--- mailer.js
+++ mailer.js
@@ -1,35 +1,21 @@
-var nodemailer = require('nodemailer');
+var sendgrid  = require('sendgrid');
 
 var contact = function (req, res) {
-  var emailAddress = req.body.email;
+
+	var emailAddress = req.body.email;
   var message = req.body.message;
 
-
-    var smtpTrans = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-          user: "*email*",
-          pass: "*password*" 
-      }
-	  });
-	  //Mail options
-	  var mailOptions = {
-	      from: emailAddress,
-	      to: 'nateconley123@gmail.com',
-	      subject: 'Website contact form',
-	      text: message
-	  };
-
-	  smtpTrans.sendMail(mailOptions, function (error, response){
-	  	if(error) {
-	  		console.log("there was an error!");
-	  		res.render('contact', { error: true });
-	  	} else {
-	  		console.log("email successfully sent!");
-	  		res.render('contact', { error: false })
-	  	}
-	  });
-  
-};
+	sendgrid.send({
+	  to:       'nateconley123@gmail.com',
+	  from:     emailAddress,
+	  subject:  'Website Contact Form',
+	  text:     message
+	}, function(err, json) {
+	  if (err) { 
+	  	return res.render('contact', { error: true }); 
+	  } 
+	  res.render('contact', { error: false })
+	});
+}
 
 module.exports.contact = contact
