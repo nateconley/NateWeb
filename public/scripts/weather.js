57a7af1d94c508dedf729082df9183a671a432fd
Nathan Conley <nateconley123@gmail.com>
2016-03-09 20:15:20 -0800

    Cleaned up Angular code significantly in weatherApp

--- public/scripts/weather.js
+++ public/scripts/weather.js
@@ -1,16 +1,19 @@
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
 angular.module('weatherApp', [])
 .controller('location', function($scope, $http){
-	// Temperature unit button classes
-	$scope.fahrenheitClass = "btn btn-primary";
-	$scope.celciusClass = "btn btn default";
-
 	// Temperature unit to fahrenheit
-	var tempUnit = "F";
+	$scope.tempUnit = "F";
+	$scope['tempUnit'] = "F"
+
 	// Set Temperature for manipulating when unit is changed
 	var temp;
 
 	// handles weather requests
-	$scope.getWeather = function(type){
+	$scope.getWeather = function(locationType){
 		// variables showing state of app for DOM elements
 		$scope.loading = true;
 		$scope.showData = false;
@@ -19,100 +22,86 @@ angular.module('weatherApp', [])
 		$scope.noGeolocation = false;
 		// empty location object
 		var location = {}
-		// If requesting zip code
-		if (type == "zip") {
+		
+     var getLocator = 'getLocation' + capitalizeFirstLetter(locationType);
+     console.log(getLocator);
+		$scope[getLocator](function(location) {
+			$scope.makeWeatherRequest(location);
+			console.log(location);
+		})
+
+  }
+
+		$scope.getLocationGeo = function(callback) {
+			if ("geolocation" in navigator) {		// Check to see if the browser supports geolocation
+				navigator.geolocation.getCurrentPosition(function(position){
+					var location = {};
+					location.latitude = position.coords.latitude;
+					location.longitude = position.coords.longitude;
+					callback(location);
+				});
+			} else {
+			// If browser doesn't support geolocation
+				$scope.noGeolocation = true;
+			}
+		}
+
+		$scope.getLocationZip = function(callback) {
+			var location = {};
 			location.zip = $scope.zipCode;
 			// Check if zip is valid (eg: 55555)
 			if (!location.zip.match(/[0-9]{5}/) || location.zip.length !== 5) {
 				$scope.invalidZip = true;
 				$scope.loading = false;
 			} else {
-				$http.post('/weather', location).then(function(response){
-					var weatherData = response.data;
-					if (!weatherData.error) {
-						// Success
-						$scope.city = weatherData.city;
-						$scope.temperature = weatherData.temperature + "º " + tempUnit;
-						temp = weatherData.temperature;
-						$scope.condition = weatherData.condition;
-						// Hide Loading Bar and show Data
-						$scope.loading = false;
-						$scope.showData = true;
-					} else {
-						// Failure
-						$scope.error = true;
-						$scope.loading = false;
-						$scope.showData = false;
-					}
-				}, function(){
+				callback(location);
+			}
+		}
+
+$scope.makeWeatherRequest = function(location) {
+			$http.post('/weather', location).then(function(response){
+				var weatherData = response.data;
+				if (!weatherData.error) {
+					// Success
+					$scope.city = weatherData.city;
+					$scope.temperature = weatherData.temperature;
+					temp = weatherData.temperature;
+					$scope.condition = weatherData.condition;
+					// Hide Loading Bar and show Data
+					$scope.loading = false;
+					$scope.showData = true;
+				} else {
 					// Failure
 					$scope.error = true;
 					$scope.loading = false;
 					$scope.showData = false;
-				});
-			}
-		} else if (type == "geo") {
-			// If requesting geoLocation
-			if ("geolocation" in navigator) {		// Check to see if the browser supports geolocation
-				navigator.geolocation.getCurrentPosition(function(position){
-					var location = {};
-					location.latitude = position.coords.latitude;
-					location.longitude = position.coords.longitude;
-					// http post function
-					$http.post('/weather', location).then(function(response){
-						var weatherData = response.data;
-						if (!weatherData.error) {
-							// Success
-							$scope.city = weatherData.city;
-							$scope.temperature = weatherData.temperature + "º " + tempUnit;
-							temp = weatherData.temperature;
-							$scope.condition = weatherData.condition;
-							// Hide Loading Bar and show Data
-							$scope.loading = false;
-							$scope.showData = true;
-						} else {
-							// Failure
-							$scope.error = true;
-							$scope.loading = false;
-							$scope.showData = false;
-						}
-					}, function(){
-						// Failure
-						$scope.error = true;
-						$scope.loading = false;
-						$scope.showData = false;
-					});
-				});
-			} else {		// If browser doesn't support geolocation
-				$scope.noGeolocation = true;
-			}
+				}
+			}, function(){
+				// Failure
+				$scope.error = true;
+				$scope.loading = false;
+				$scope.showData = false;
+			});
 		}
-	}
 
 	$scope.changeUnit = function(unit){
 
-		// Change button class for selected button
-		if (unit == "fahrenheit") {
-			// Change button class for selected button
-			$scope.fahrenheitClass = "btn btn-primary";
-			$scope.celciusClass = "btn btn default";
-			if (tempUnit == "C"){
-				tempUnit = "F";
-				$scope.temperature = temp + "º " + tempUnit;
-			}
-
-		} else if (unit == "celcius") {
-			// Change button class for selected button
-			$scope.fahrenheitClass = "btn btn-default";
-			$scope.celciusClass = "btn btn-primary";
-			if (tempUnit == "F"){
-				var tempCelcius = (temp - 32) * 0.555555555;
-				tempCelcius = Math.round(tempCelcius);
-				tempUnit = "C";
-				$scope.temperature = tempCelcius + "º " + tempUnit;
-			}	
+		if (unit === $scope.tempUnit) {
+			return;
 		}
+		if (unit === "F") {
+
+				$scope.temperature = temp;
+		} else if (unit == "C") {
+			var tempCelcius = (temp - 32) * 0.555555555;
+			tempCelcius = Math.round(tempCelcius);
+			$scope.temperature = tempCelcius;
+		}
+		$scope.tempUnit = unit
 	}
 
-});
 
+
+
+
+});
